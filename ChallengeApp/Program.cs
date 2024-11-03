9fa8f0d101c72b0b7722cd62de9f41ebb9a7b8ac
maciejdelfin <maciejdelfin@hotmail.com>
2024-11-03 20:10:59 +0100

    Dzień 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello");
+﻿using System.Collections.Concurrent;
+
+var name = "Ewa";
+var sex = "female";
+var age = 30;
+
+if (sex == "female" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa lat 30");
+}
+else if (sex != "female" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else if (sex != "female" && age == 10)
+{
+    Console.WriteLine("Chłopiec w wieku 10 lat");
+}
+
+
+
