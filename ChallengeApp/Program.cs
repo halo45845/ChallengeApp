df1e977b3ffb9a9adc3a98e0ce85c0cfd09f1ee6
maciejdelfin <maciejdelfin@hotmail.com>
2025-01-19 13:00:28 +0100

    Dzien 5 T|ablice

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,25 +1,17 @@
-﻿using System.Collections.Concurrent;
+﻿// Dzień 5 - Tablice i Listy
 
-var name = "Ewa";
-var sex = "female";
-var age = 30;
-
-if (sex == "female" && age < 30)
+List<string> dayOfWeeks = new List<string>
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
-else if (name == "Ewa" && age == 30)
-{
-    Console.WriteLine("Ewa lat 30");
-}
-else if (sex != "female" && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
-else if (sex != "female" && age == 10)
-{
-    Console.WriteLine("Chłopiec w wieku 10 lat");
-}
+    "Poniedziałek",
+    "Wtorek",
+    "Środa",
+    "Czwartek",
+    "Piątek",
+    "Sobota",
+    "Niedziela"
+};
 
+for (var i = 0; i < dayOfWeeks.Count; i++)
+    Console.WriteLine(dayOfWeeks[i]);
 
-
+//foreach (var dayOfWeek in dayOfWeeks)
